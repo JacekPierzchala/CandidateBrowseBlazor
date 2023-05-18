1f1b48085acaf586d8fb0e5b10492cd6d2495887
Jacek Placek <jacekpierzchala12@gmail.com>
2023-05-18 13:18:04 +0200

    feat: implemented elements wrapping and filter notification

---
+++ CandidateBrowserCleanArch.Blazor.WASM/wwwroot/scripts/windowScripts.js
@@ -0,0 +1,19 @@
+﻿ function showAlert(message) {
+     alert(window.innerWidth);
+}
+
+function getWindowDimensions() {
+    //windowDimensions = function () {
+    //    return {
+    //        width: window.innerWidth,
+    //        height: window.innerHeight
+    //    };
+    //};
+
+    return {
+        'width': window.innerWidth,
+        'height': window.innerHeight,
+    }
+
+
+}
